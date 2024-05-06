87f5b6f34c24b0a20c14464beec330fdb89eb5d0
Baraa Farek <BaraaFarek@gmail.com>
2024-05-07 02:59:29 +0300

    changes before saja 2

--- lib/layout/main_layout/Home_Screen.dart
+++ lib/layout/main_layout/Home_Screen.dart
@@ -26,50 +26,53 @@ class home1 extends StatelessWidget {
         listener: (BuildContext context, app_states state) {},
         builder: (BuildContext context, app_states state) {
           var cubit = app_cubit.get(context);
-          return Scaffold(
-            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-            appBar: AppBar(
-              backgroundColor: cubit.isdark ? Colors.grey[500] : Colors.white,
-              elevation: 0.0,
-              actions: [
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.search,
-                      color: cubit.isdark ? Colors.white : Colors.black,
-                    )),
-                IconButton(
-                    onPressed: () {
-                      app_cubit.get(context).changeMode();
-                    },
-                    icon: Icon(
-                      cubit.isdark ? Icons.sunny : Icons.brightness_3_sharp,
-                      color: cubit.isdark ? Colors.white : Colors.black,
-                    )),
-              ],
-              title: Text(
-                'Ballon',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'ITCKRIST.TTF',
-                  color: cubit.isdark ? Colors.white : Colors.purpleAccent,
+          return SafeArea(
+            child: Scaffold(
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+              appBar: AppBar(
+                backgroundColor: cubit.isdark ? Colors.grey[500] : Colors.white,
+                elevation: 0.0,
+                actions: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.search,
+                        color: cubit.isdark ? Colors.white : Colors.black,
+                      )),
+                  IconButton(
+                      onPressed: () {
+                        app_cubit.get(context).changeMode();
+                      },
+                      icon: Icon(
+                        cubit.isdark ? Icons.sunny : Icons.brightness_3_sharp,
+                        color: cubit.isdark ? Colors.white : Colors.black,
+                      )),
+                ],
+                title: Text(
+                  ' Ballon',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 32.0,
+                    fontFamily: 'ITCKRIST.TTF',
+                    color: cubit.isdark ? Colors.white : Colors.purpleAccent,
+                  ),
                 ),
               ),
-            ),
-            body: cubit.screens[cubit.currentindex],
-            bottomNavigationBar: BottomNavigationBar(
-              items: cubit.bottomItems,
-              backgroundColor: cubit.isdark ? Colors.grey : Colors.white,
-              // fixed  لما بنكبس على وحدة ما بتغير الباقي
-              type: BottomNavigationBarType.fixed,
-              showSelectedLabels: true,
-              selectedItemColor: Colors.purple,
-              unselectedItemColor: cubit.isdark ? Colors.white : Colors.grey,
-              showUnselectedLabels: false,
-              currentIndex: cubit.currentindex,
-              onTap: (index) {
-                cubit.changeIndex(index);
-              },
+              body: cubit.screens[cubit.currentindex],
+              bottomNavigationBar: BottomNavigationBar(
+                items: cubit.bottomItems,
+                backgroundColor: cubit.isdark ? Colors.grey : Colors.white,
+                // fixed  لما بنكبس على وحدة ما بتغير الباقي
+                type: BottomNavigationBarType.fixed,
+                showSelectedLabels: true,
+                selectedItemColor: Colors.purple,
+                unselectedItemColor: cubit.isdark ? Colors.white : Colors.grey,
+                showUnselectedLabels: false,
+                currentIndex: cubit.currentindex,
+                onTap: (index) {
+                  cubit.changeIndex(index);
+                },
+              ),
             ),
           );
         },
